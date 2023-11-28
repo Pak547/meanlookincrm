e57b5243cfc0bea72dc790d3a5837fefb379756f
Pak <pakapongm@gmail.com>
2023-11-27 17:02:56 -0800

    created possible routes to get, create, update, delete, with specific params for id

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 5000;
 
 app.use("/api/contacts", require("./routes/contactRoutes"))
 
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 });
