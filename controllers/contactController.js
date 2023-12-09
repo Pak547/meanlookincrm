374a101f54698891fdaccc2e07c5983de0e18cf7
Pak <pakapongm@gmail.com>
2023-12-08 20:01:13 -0800

    added and installed asychandler to handle errors

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,8 +1,13 @@
-const getAllContact = (req, res) => {
-    res.status(200).json({message: `Get all contacts`});
-};
+const asyncHandler = require('express-async-handler');
+// install asynchandler
+// wrap handler in async so we dont have to write a try catch block in order to catch an order
+// handler automatically when exception has occured passes to handler
 
-const createContact = (req, res) => {
+const getAllContact = asyncHandler(async(req, res) => {
+    res.status(200).json({message: `Get all contacts`});
+});
+
+const createContact = asyncHandler(async(req, res) => {
     console.log("the req body is", req.body)
     res.status(200).json({message: `Create contacts`});
     const {name, email, phone} = req.body;
@@ -10,18 +15,19 @@ const createContact = (req, res) => {
         res.status(400);
         throw new Error("All fields are mandatory")
     }
-};
+    res.status(201).json({ message: "Create Contact"});
+});
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Get contact for ${req.params.id}`});
-};
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Create contacts`});
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async(req, res) => {
     res.status(200).json({message: `Delete contacts for ${req.params.id}`});
-};
+});
 
 module.exports = { getAllContact, createContact, getContact, updateContact, deleteContact }
